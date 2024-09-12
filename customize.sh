a030b756cc3d5b1d083517e99ac20773de5b056d
Cui <530051970@qq.com>
2024-09-12 10:57:08 +0800

    update shell script

--- customize.sh
+++ customize.sh
@@ -13,7 +13,7 @@ ASSET_STACK_NAME=$2
 # Modify package name
 cd ..
 mv "auth-hub-demo" $ASSET_PACKAGE_NAME
-cd $ASSET_PACKAGE_NAME"
+cd $ASSET_PACKAGE_NAME
 
 sed -i '' "s/auth-hub-demo-stack/${ASSET_PACKAGE_NAME}/g" bin/main.ts
 sed -i '' "s/AuthHubDemoStack/${ASSET_STACK_NAME}/g" bin/main.ts
@@ -23,6 +23,3 @@ mv lib/auth-hub-demo-stack.ts lib/${ASSET_PACKAGE_NAME}.ts
 
 sed -i '' "s/auth-hub-demo/${ASSET_PACKAGE_NAME}/g" lib/constant.ts
 sed -i '' "s/Authkit/${ASSET_STACK_NAME}/g" lib/constant.ts
-
-
-
